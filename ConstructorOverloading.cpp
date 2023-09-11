c7c3e3ab97b57245167c77c7725e4db6f6510e6d
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-09-11 15:26:40 +0530

    Manipulated ConstructorOverloading

--- ConstructorOverloading.cpp
+++ ConstructorOverloading.cpp
@@ -3,34 +3,6 @@
 // 2. Different number of parameters
 // 3. Different type of parameters
 
-// More than one constructor in same class, but with different parameters
-// #include <iostream>
-// #include <string>
-// #include <conio.h>
-// using namespace std;
-
-// class Person {
-// public:
-//     string name;
-//     int age;
-//     Person(string n, int a) {
-//         name = n;
-//         age = a;
-//     }
-//     Person() {
-//         name = "noname";
-//         age = 0;
-//     }
-// };
-
-// int main() {
-//     Person person1("David", 21);
-//     Person person2;
-//     cout << person1.name << " " << person1.age << endl;
-//     cout << person2.name << " " << person2.age << endl;
-//     return 0;
-// }
-
 // Constructor Overloading using only a single object
 #include <iostream>
 #include <string>
@@ -57,6 +29,9 @@ int main() {
     // Another parameter in same object
     person1 = Person("John", 25);
     cout << person1.name << " " << person1.age << endl;
+    // Why person1 = Person("John", 25); is used and not Person person1("John", 25); ?
+    // Because, Person person1("John", 25); is used to create a new object, but we want to change the value of the same object
+
 
     Person person2;
     cout << person2.name << " " << person2.age << endl;
