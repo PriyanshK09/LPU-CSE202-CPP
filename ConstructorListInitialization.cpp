901db4ca63ee74ce4377269924110d01edb86151
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-09-12 15:31:56 +0530

    Updated with another example of Initialization List in Constrcutor

--- ConstructorListInitialization.cpp
+++ ConstructorListInitialization.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class rectangle{
-    int l,b;
+// class rectangle{
+//     int l,b;
+//     public:
+//         rectangle(int x=12, int y=34):l(x),b(y){
+//             cout<<"Constructor called"<<endl;
+//         }
+//         int area(){
+//             return l*b;
+//         }
+// };
+// int main()
+// {
+//     rectangle r;
+//     cout<<"Area is "<<r.area()<<endl;
+//     rectangle r1(10,20);
+//     cout<<"Area is "<<r1.area()<<endl;
+// }
+
+class Base
+{
+    private:
+        int value;
     public:
-        rectangle(int x=12, int y=34):l(x),b(y){
-            cout<<"Constructor called"<<endl;
-        }
-        int area(){
-            return l*b;
-        }
+    Base (int value):value(value)
+    {
+        cout<<"Value is "<<value<<endl;
+    }
 };
+
 int main()
 {
-    rectangle r;
-    cout<<"Area is "<<r.area()<<endl;
-    rectangle r1(10,20);
-    cout<<"Area is "<<r1.area()<<endl;
+    Base b(10);
+    return 0;
 }
