a56bbc97bda5e54ca98823e0850f467cc1134cf7
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-09-18 15:11:23 +0530

    Repository modified

--- FileHandling-Read.cpp
+++ FileHandling-Read.cpp
@@ -4,17 +4,23 @@ using namespace std;
 
 int main()
 {
-    ifstream rf("result.txt");
-    string line;
-    // Without Loop
-    getline(rf, line);
-    cout << line << endl;
+    // ifstream rf("result.txt");
+    // string line;
+    // // Without Loop
+    // getline(rf, line);
+    // cout << line << endl;
 
     // With Loop
     // while (getline(of, line))
     // {
     //     cout << line << endl;
     // }
+
+    char s[10];
+    ifstream inf("result.txt");
+    inf >> s;
+    inf.close();
+    cout << s << endl;
 
     return 0;
 }
