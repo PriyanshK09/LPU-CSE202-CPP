1a646a41b4af5d389fbf266fc3605654e5224121
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-10-30 15:45:17 +0530

    method updated

--- SelfRefrential-CostOfItems.cpp
+++ SelfRefrential-CostOfItems.cpp
@@ -1,22 +1,25 @@
-// The store offers main categories of items : Wooden Items and Electronics.
-// As part of the system you are required to create a program that calculates
-// the Total Amount a customer needs to pay based on the type and quantity.
+/*
+The store offers main categories of items : Wooden Items and Electronics.
+As part of the system you are required to create a program that calculates
+the Total Amount a customer needs to pay based on the type and quantity.
 
-// Item Type (Abstract Base Class)
-// This is an abstract base class that defines pure virtual function calculateAmount() which calculates the total amount of the items to be purchased.
-// It acts as a blueprint for the derived classes.
-// Wooden (Derived from Item Type)
-// This class represents wooden items available in the store. It has two private data members : 
-// numberOfItems (representing the quantity of the item) and cost (representing the cost of each item).
-// The class implements the calculateAmount() function by multiplying the numberOfItems with the cost.
-// Electronics (Derived from Item Type)
-// This class represents electronic items available in the store. It has one private data members :
-// cost (representing the cost of each item). The class implements the calculateAmount() function by applying a 20% discount on the cost of the item.
+Item Type (Abstract Base Class)
+This is an abstract base class that defines pure virtual function calculateAmount() which calculates the total amount of the items to be purchased.
+It acts as a blueprint for the derived classes.
+Wooden (Derived from Item Type)
+This class represents wooden items available in the store. It has two private data members : 
+numberOfItems (representing the quantity of the item) and cost (representing the cost of each item).
+The class implements the calculateAmount() function by multiplying the numberOfItems with the cost.
+Electronics (Derived from Item Type)
+This class represents electronic items available in the store. It has one private data members :
+cost (representing the cost of each item). The class implements the calculateAmount() function by applying a 20% discount on the cost of the item.
 
-// Input Format :
-// First line of input consists of an integer representing the choice
-// If choice is 1, enter the numbers of items and their cost
-// If choice is 2, enter the cost
+Input Format :
+First line of input consists of an integer representing the choice
+If choice is 1, enter the numbers of items and their cost
+If choice is 2, enter the cost
+*/
+
 
 #include <iostream>
 using namespace std;
