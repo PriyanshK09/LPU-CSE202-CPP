6f9cee5344c2068800bdb0364c3f37692e30ad7e
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-11-07 14:56:10 +0530

    modified:   STL-Iterator.cpp

--- STL-Iterator.cpp
+++ STL-Iterator.cpp
@@ -7,7 +7,8 @@ int main()
     int arr[] = {12, 3, 17, 8};
     vector<int> v(arr, arr + 4);
     for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-        cout << *it << " ";
+        cout << *it << " "; // De-refrencing iterator to get value
+                            // Value of the element pointed by iterator
 
     return 0;
 }
