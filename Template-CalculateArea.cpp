eb8fd8818ee9c61d8fd12d6913a2d4997160b846
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-11-07 15:28:26 +0530

    Added multiple parameter

--- Template-CalculateArea.cpp
+++ Template-CalculateArea.cpp
@@ -6,6 +6,13 @@ T square(T num)
     return num * num;
 }
 
+template <class T, class U>
+// Multiplication of 2 Different Data Types
+T multiply(T floatNum, U intNum)
+{
+    return floatNum * intNum;
+}
+
 int main()
 {
     int num1 = 3;
@@ -14,6 +21,8 @@ int main()
     cout << "\nSquare of " << num2 << " is " << square<float>(num2);
     double num3 = 123.456;
     cout << "\nSquare of " << num3 << " is " << square<double>(num3);
+
+    cout << "\n\nMultiplication of " << num1 << " and " << num2 << " is " << multiply<int, float>(num1, num2);
 
     return 0;
 }
